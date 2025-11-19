238f7ee9460bbd9230374d1dfdfba83be23d4d47
Mohammed Alfakir <its.mohammedalfakir@gmail.com>
2025-11-19 14:47:57 +0100

    Using DoorModels Package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import DoorModels from "door_models";
 
 function App() {
+  const doorMaterials = {
+    D_SRF_DECOR_2:
+      "https://threejs.org/examples/textures/hardwood2_diffuse.jpg",
+    D_SRF_DECOR: "#A52A2A",
+    D_GASKET: "#1A1A1A",
+    D_FR_COLOR: { value: "#B0E0E6", opacity: 0.5 },
+    Body: "#8F7470",
+    D_PRF_COLOR: "#4C4A48",
+    Hinge: "#C0C0C0",
+    HingeCuts: "#A9A9A9",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      camera={{ fov: 50, position: [0, 0, 5] }}
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      {/* Light */}
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 10, 7.5]} intensity={1} castShadow />
+      {/* Door Model */}
+      <DoorModels
+        doorName="P_SD_FH_AF40_40OCC"
+        materials={doorMaterials}
+        totalWidth={1000}
+        totalHeight={2400}
+        totalDepth={120}
+        doorPivot="left"
+        doorOpening="out"
+      />
+      <OrbitControls makeDefault />
+    </Canvas>
   );
 }
 
